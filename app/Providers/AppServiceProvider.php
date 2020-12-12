f8e6cdcc2282f3521cc5e8385e2511921d67bce1
japanid133 <baguspw2405@gmail.com>
2020-12-12 21:07:13 +0700

    SSL support setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,16 +15,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (env('ENABLE_SSL') === 'TRUE') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
      * Bootstrap any application services.
      *
+     * @param UrlGenerator $url
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+        if (env('ENABLE_SSL') === 'TRUE') {
+            $url->formatScheme('https');
+        }
+        Schema::defaultStringLength(190);
     }
 }
