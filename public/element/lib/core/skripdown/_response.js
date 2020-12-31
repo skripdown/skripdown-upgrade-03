15e6f2f54c067fdd9ed7bdf3a108e1687ac6baa8
japanid133 <baguspw2405@gmail.com>
2020-12-31 11:53:05 +0700

    _response token decrypt bug fix

--- public/element/lib/core/skripdown/_response.js
+++ public/element/lib/core/skripdown/_response.js
@@ -15,23 +15,42 @@ window._response = {
     },
     response : undefined,
     post : function (input) {
+        if (input.file !== undefined) {
+            const formData = new FormData();
+            const keys = Object.keys(input.data);
+            for (let i = 0; i < keys.length; i++) {
+                if (input.file[keys[i]] !== undefined)
+                    formData.append(keys[i],input.file[keys[i]]);
+                else
+                    formData.append(keys[i],input.data[keys[i]]);
+            }
+            input.data = formData;
+        }
         if (input.async === undefined)
             input.async = true;
         if (input.enctype === undefined)
             input.enctype = 'multipart/form-data';
         if (input.processData === undefined)
             input.processData = false;
+        if (input.contentType === undefined)
+            input.contentType = false;
         let csrft    = this.csrft;
         csrft = csrft.slice(6,csrft.length-5);
         csrft = csrft.split('');
         csrft = csrft.reverse('');
         csrft = csrft.join('');
-        input.data._token = csrft;
+        if (input.file !== undefined)
+            input.data.append('_token',csrft);
+        else
+            input.data._token = csrft;
+        console.log(input.data);
         $.ajax({
             type : 'post',
             async : input.async,
             url  : input.url,
             enctype : input.enctype,
+            processData : input.processData,
+            contentType : input.contentType,
             data : input.data,
             success : e=>{
                 _response.response = e;
