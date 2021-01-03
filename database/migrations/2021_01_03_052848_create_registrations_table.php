10662da89b10ba84cca9ed9a03f265cef30c7d92
japanid133 <baguspw2405@gmail.com>
2021-01-04 06:08:49 +0700

    modify registrations model

---
+++ database/migrations/2021_01_03_052848_create_registrations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistrationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registrations', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('super_id')->unsigned();
+            $table->string('link')->nullable();
+            $table->timestamps();
+            $table->foreign('super_id')
+                ->references('id')
+                ->on('supers')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registrations');
+    }
+}
