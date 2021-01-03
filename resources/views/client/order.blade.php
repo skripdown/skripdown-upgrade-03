9b1288bffbc3e17454812c27b9ed1c6882b74edb
japanid133 <baguspw2405@gmail.com>
2021-01-04 06:10:47 +0700

    update system views

--- resources/views/client/order.blade.php
+++ resources/views/client/order.blade.php
@@ -10,11 +10,25 @@
             <h2 class="card-title">
                 Order
             </h2>
-            <h6 class="card-title text-black-50">{{$order->token}}</h6>
+            <h6 class="card-title text-black-50"><a href="{{asset(env('PATH_CLIENT_TRANSACTION').$order->transaction)}}" target="_blank">{{$order->token}}</a></h6>
             @if ($order->verified)
                 <h6 class="card-title text-success">terverifikasi</h6>
             @else
-                <h6 class="card-title text-warning">menunggu verifikasi</h6>
+                @if (\Illuminate\Support\Facades\Auth::check())
+                    @if (\Illuminate\Support\Facades\Auth::user()->role == 'developer')
+                        @if (\Illuminate\Support\Facades\Auth::user()->developer->role == 'admin')
+                            <h6 class="card-title text-danger">
+                                belum terverifikasi
+                                <span class="btn btn-sm btn-info ml-3" type="button" data-toggle="modal" data-target="#popup_verify_agree">verifikasi</span>
+                                <span class="btn btn-sm btn-danger ml-1" type="button" data-toggle="modal" data-target="#popup_verify_cancel">batal</span>
+                            </h6>
+                        @else
+                            <h6 class="card-title text-warning">menunggu verifikasi admin</h6>
+                        @endif
+                    @endif
+                @else
+                    <h6 class="card-title text-warning">menunggu verifikasi</h6>
+                @endif
             @endif
             <hr>
             <form>
@@ -58,3 +72,75 @@
         </div>
     </div>
 @endsection
+
+@if (\Illuminate\Support\Facades\Auth::check())
+    @if (\Illuminate\Support\Facades\Auth::user()->role == 'developer')
+        @if (\Illuminate\Support\Facades\Auth::user()->developer->role == 'admin')
+@section('pop-up')
+    <div id="popup_verify_agree"></div>
+    <div id="popup_verify_cancel"></div>
+@endsection
+
+@section('script-body')
+    <script src="{{asset(env('LIB_PATH').'core/skripdown/_dev.js')}}"></script>
+    <script src="{{asset(env('LIB_PATH').'core/skripdown/_data.js')}}"></script>
+    <script src="{{asset(env('LIB_PATH').'core/skripdown/_enc.js')}}"></script>
+    <script src="{{asset(env('LIB_PATH').'core/skripdown/_response.js')}}"></script>
+    <script src="{{asset(env('LIB_PATH').'core/skripdown/_ui_factory.js')}}"></script>
+    <script>
+        @include('root.token')
+        console.log('{{csrf_token()}}');
+        _dev.order.insert({!! $order !!});
+        const a = ()=>{
+            const ctr     = document.createElement('div');
+            const btn_ver = document.createElement('a');
+            const parag   = document.createElement('p');
+            btn_ver.innerHTML   = 'verifikasi';
+            parag.innerHTML   = 'Apakah anda yakin ingin memverifikasi order ini ? ';
+            btn_ver.setAttribute('class','btn btn-sm btn-success float-right text-white');
+            btn_ver.setAttribute('type','button');
+            btn_ver.addEventListener('click', function () {
+                _response.post({url:'{{url('verifyOrder')}}',data:{id:'{{$order->id}}'}});
+                if (_response.response._status)
+                    close();
+            });
+            ctr.append(parag);
+            ctr.append(btn_ver);
+
+            return [ctr];
+        }
+        const b = ()=>{
+            const ctr     = document.createElement('div');
+            const btn_ver = document.createElement('a');
+            const parag   = document.createElement('p');
+            btn_ver.innerHTML   = 'tolak';
+            parag.innerHTML   = 'Apakah anda yakin ingin menolak order ini ? ';
+            btn_ver.setAttribute('class','btn btn-sm btn-secondary float-right text-white');
+            btn_ver.setAttribute('type','button');
+            ctr.append(parag);
+            ctr.append(btn_ver);
+            btn_ver.addEventListener('click', function () {
+                _response.post({url:'{{url('cancelOrder')}}',data:{id:'{{$order->id}}'}});
+                if (_response.response._status)
+                    close();
+            });
+
+            return [ctr];
+        }
+        const res_a = a();
+        const res_b = b();
+        _popup.render({
+            element:'popup_verify_agree',
+            title:'<strong>verifikasi order</strong>',
+            content:res_a[0],
+        });
+        _popup.render({
+            element:'popup_verify_cancel',
+            title:'<strong>tolak order</strong>',
+            content:res_b[0],
+        });
+    </script>
+@endsection
+@endif
+@endif
+@endif
