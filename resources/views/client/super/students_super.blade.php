be5a00c8b6df2e8a80a76ab0033ee65690d8ad37
japanid133 <baguspw2405@gmail.com>
2021-01-08 22:11:25 +0700

    init super feature view

---
+++ resources/views/client/super/students_super.blade.php
@@ -0,0 +1,48 @@
+@extends('client.template')
+
+@section('title')
+    Penulis
+@endsection
+
+@section('sidebar-menu')
+    <li class="sidebar-item">
+        <a class="sidebar-link has-arrow" href="javascript:void(0)" aria-expanded="false">
+            <i data-feather="users" class="feather-icon"></i>
+            <span class="hide-menu">Akun Pengguna</span>
+        </a>
+        <ul aria-expanded="false" class="collapse  first-level base-level-line">
+            <li class="sidebar-item">
+                <a href="{{url('/students')}}" class="sidebar-link">
+                    <span class="hide-menu"> Penulis</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="{{url('/advisors')}}" class="sidebar-link">
+                    <span class="hide-menu"> Dosen</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="{{url('/departments')}}" class="sidebar-link">
+                    <span class="hide-menu"> Jurusan</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="{{url('/faculties')}}" class="sidebar-link">
+                    <span class="hide-menu"> Fakultas</span>
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li class="sidebar-item">
+        <a class="sidebar-link sidebar-link" href="{{url('/docs')}}" aria-expanded="false">
+            <i data-feather="file-text" class="feather-icon"></i>
+            <span class="hide-menu">Dokumen</span>
+        </a>
+    </li>
+    <li class="sidebar-item">
+        <a class="sidebar-link sidebar-link" href="{{url('/api')}}" aria-expanded="false">
+            <i data-feather="code" class="feather-icon"></i>
+            <span class="hide-menu">API</span>
+        </a>
+    </li>
+@endsection
